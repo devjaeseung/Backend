1e6fb5fedd5efb98e2c1b2e32311fb0fd000b1ce
devjaeseung <dev.jaeseung@gmail.com>
2025-02-19 15:42:29 +0900

    first commit

---
+++ Websocket-Module/src/main/kotlin/com/example/websocketmodule/WebsocketModuleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.websocketmodule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketModuleApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketModuleApplication>(*args)
+}
---
+++ Websocket-Module/src/test/kotlin/com/example/websocketmodule/WebsocketModuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.websocketmodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketModuleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
