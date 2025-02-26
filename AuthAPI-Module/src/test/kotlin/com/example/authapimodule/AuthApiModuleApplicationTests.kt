ef14a106d53945bef27a3e00ea9eba9918e7fd31
devjaeseung <dev.jaeseung@gmail.com>
2025-02-26 19:36:42 +0900

    feat: 회원가입/로그인기능 개발 시작

---
+++ AuthAPI-Module/src/test/kotlin/com/example/authapimodule/AuthApiModuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.authapimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthApiModuleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
